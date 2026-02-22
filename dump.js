816b30695d0396ad6aa416f959e8c83c241908ef
Livingston <livingston@users.noreply.github.com>
2026-02-22 15:50:45 +0530

    onetab datadump-1.1 v3

--- dump.js
+++ dump.js
@@ -1,10 +1,11 @@
 (async () => {
   const contentDiv = document.getElementById('content');
-  chrome.storage.local.get('state', ({state}) => {
-  const text = state ? JSON.parse(state)
-    .tabGroups.map(g=>g.tabsMeta)
-    .map(tabs=>tabs.map(t=>`${t.url} | ${t.title}`).join('\n'))
-    .join('\n\n') : 'No data found';
+  const { state } = await chrome.storage.local.get('state');
+  const text = state
+    ? JSON.parse(state)
+        .tabGroups.map((g) => g.tabsMeta)
+        .map((tabs) => tabs.map((t) => `${t.url} | ${t.title}`).join('\n'))
+        .join('\n\n')
+    : 'No data found';
   contentDiv.appendChild(document.createTextNode(text));
-  });
 })();
